dcbc47bc63ae37c0690d89d99a27b12da971f353
srinath39 <96327215+srinath39@users.noreply.github.com>
2025-02-20 20:55:20 +0530

    Time: 6 ms (48.34%) | Memory: 43 MB (76.76%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,20 +1,41 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if(s.equals(" ")){
-            return true;
-        }
-        String str=s.toLowerCase().replaceAll("[^a-z0-9]","");
-        int n=str.length();
-        if(n==0 || n==1){
-            return true;
-        }
+        int n=s.length();
         int i=0;
-        while(i<n/2){
-            if(!(str.charAt(i)==str.charAt(n-i-1))){
-                return false;
+        int j=n-1;
+        while(i<j){
+            char c1=s.charAt(i);
+            char c2=s.charAt(j);
+            if(isAlphaNumeric(c1)&&isOtherChar(c2)){
+                j--;
             }
-            i++;
+            else if(isOtherChar(c1)&&isAlphaNumeric(c2)){
+                i++;
+            }
+            else{
+                if(isAlphaNumeric(c1)&&isAlphaNumeric(c2)){
+                    if(!(Character.toLowerCase(c1)==Character.toLowerCase(c2))){
+                        return false;
+                    }
+                }
+                i++;
+                j--;
+            }
         }
         return true;
+    }
+
+    public static boolean isAlphaNumeric(char c){
+        if(Character.isDigit(c) || Character.isLetter(c)){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isOtherChar(char c){
+        if(Character.isWhitespace(c) || !isAlphaNumeric(c)){
+            return true;
+        }
+        return false;
     }
 }
