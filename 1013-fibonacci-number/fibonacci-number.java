c210fcbb14814a0e2f0cbce446882702e66b4d23
srinath39 <96327215+srinath39@users.noreply.github.com>
2025-02-21 01:07:43 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (62.54%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,16 +1,8 @@
 class Solution {
     public int fib(int n) {
-       if(n<=1){
-        return n;
-       }
-       int a,b,c;
-       a=0;
-       b=1;
-       for(int i=2;i<=n;i++){
-            c=a+b;
-            a=b;
-            b=c;
-       }
-       return b;
+        double sqrt5=Math.sqrt(5);
+        double dividend=Math.pow((1+sqrt5)/2,n)-Math.pow((1-sqrt5)/2,n);
+        double val=dividend/sqrt5;
+        return (int)val;
     }
 }
