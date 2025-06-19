5df999378815b3fe8a9ebd8b4de6ea28da9cd7b4
srinath39 <96327215+srinath39@users.noreply.github.com>
2025-06-19 08:22:34 +0530

    Time: 21 ms (26.72%) | Memory: 45.3 MB (45.85%) - LeetSync

---
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -0,0 +1,48 @@
+class Solution {
+    public Map<Integer,LinkedList<Integer>> getGraphData(int V,int[][] edges){
+        Map<Integer,LinkedList<Integer>> map=new HashMap<>();
+        for(int i=0;i<V;i++){
+            map.put(i,new LinkedList<>());
+        }
+        for(int i=0;i<edges.length;i++){
+            map.get(edges[i][0]).add(edges[i][1]);
+            map.get(edges[i][1]).add(edges[i][0]);
+        }
+        return map;
+    }
+
+    public int countCompleteComponents(int n, int[][] edges) {
+        Map<Integer,LinkedList<Integer>> map=getGraphData(n,edges);
+        int count=0;
+        boolean[] bool=new boolean[n];
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<n;i++){
+            if(bool[i]){
+                continue;
+            }
+            int no_of_edges=0;
+            int no_of_nodes=0;
+            st.push(i);
+            while(!st.empty()){
+                int val=st.pop();
+                if(bool[val]){
+                    continue;
+                }
+                bool[val]=true;
+                no_of_nodes++;
+                LinkedList<Integer> adjList=map.get(val);
+                for(int j=adjList.size()-1;j>=0;j--){
+                    int ele=adjList.get(j);
+                    if(!bool[ele]){
+                        st.push(ele);
+                        no_of_edges++;
+                    }
+                }
+            }
+            if(((no_of_nodes*(no_of_nodes-1))/2)==no_of_edges){
+                count++;
+            }
+        }
+        return count;
+    }
+}
