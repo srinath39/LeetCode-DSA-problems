1cb82bca28acd9e1850c9000f5b9c4ba005b0bdd
srinath39 <96327215+srinath39@users.noreply.github.com>
2025-06-27 18:10:53 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (29.97%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,73 +2,56 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        ArrayList<int[]> list=new ArrayList<>();
-        int[][] udist=new int[n][m];
+        Queue<int[]> q=new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    list.add(new int[]{i,j});
+                    q.add(new int[]{i,j});
                 }
-                udist[i][j]=Integer.MAX_VALUE;
             }
         }
-
-        boolean[][] ubool=new boolean[n][m];
-        for(int i=0;i<list.size();i++){
-            int[] rotten=list.get(i);
-            BFS(rotten,grid,udist,ubool);
-        }
-        if(isFreshOrangeLeft(ubool,grid)){
+        int t=BFS(grid,q);
+        if(isFreshOrangeLeft(grid)){
             return -1;
         }
-        return minMinutes(udist,grid);
+        if(t==-1){
+            return 0;
+        }
+        return t;
     }
 
-    public boolean isFreshOrangeLeft(boolean[][] bool,int[][] grid){
+    public int BFS(int[][] grid,Queue<int[]> q){
+        int n=grid.length;
+        int m=grid[0].length;
+        int t=-1;
+        int[] dr={-1,1,0,0};
+        int[] dc={0,0,-1,1};
+        while(!q.isEmpty()){
+            t++;
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                int[] cur=q.remove();
+                for(int j=0;j<4;j++){
+                    int x=cur[0]+dr[j];
+                    int y=cur[1]+dc[j];
+                    if(x>=0 && x<=n-1 && y>=0 && y<=m-1 && grid[x][y]==1){
+                        q.add(new int[]{x,y});
+                        grid[x][y]=2;
+                    }
+                }
+            }
+        }
+        return t;
+    }
+
+    public boolean isFreshOrangeLeft(int[][] grid){
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1 && !bool[i][j]){
+                if(grid[i][j]==1){
                     return true;
                 }
             }
         }
         return false;
-    }
-
-    public int minMinutes(int[][] dist,int[][] grid){
-        int max=0;
-        for(int i=0;i<dist.length;i++){
-            for(int j=0;j<dist[0].length;j++){
-                if(grid[i][j]==1){
-                    max=Math.max(max,dist[i][j]);
-                }
-            }
-        }
-        return max;
-    }
-
-    public void BFS(int[] rotten,int[][] grid,int[][] udist,boolean[][] ubool){
-        int n=grid.length;
-        int m=grid[0].length;
-        Queue<int[]> q=new LinkedList<>();
-        q.add(rotten);
-        int[][] dist=new int[n][m];
-        boolean[][] bool=new boolean[n][m];
-        int[] dr={-1,1,0,0};
-        int[] dc={0,0,-1,1};
-        while(!q.isEmpty()){
-            int[] cur=q.remove();
-            for(int i=0;i<4;i++){
-                int x=cur[0]+dr[i];
-                int y=cur[1]+dc[i];
-                if(x>=0 && x<=n-1 && y>=0 && y<=m-1 && (!bool[x][y]) && grid[x][y]==1){
-                    q.add(new int[]{x,y});
-                    dist[x][y]=dist[cur[0]][cur[1]]+1;
-                    bool[x][y]=true;
-                    ubool[x][y]=true;
-                    udist[x][y]=Math.min(udist[x][y],dist[x][y]);
-                }
-            }
-        }
     }
 }
