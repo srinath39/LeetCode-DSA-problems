94c89c57d4acbbacc61b66678717d9b0c0235579
srinath39 <96327215+srinath39@users.noreply.github.com>
2025-06-27 20:49:38 +0530

    Time: 14 ms (61.60%) | Memory: 47.4 MB (57.75%) - LeetSync

---
+++ 542-01-matrix/01-matrix.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int n=mat.length;
+        int m=mat[0].length;
+        int[][] dist=new int[n][m];
+        Queue<int[]> queue=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    queue.add(new int[]{i,j});
+                }
+            }
+        }
+        int[] dr={-1,1,0,0};
+        int[] dc={0,0,-1,1};
+        while(!queue.isEmpty()){
+            int sz=queue.size();
+            for(int i=0;i<sz;i++){
+                int[] cur=queue.remove();
+                for(int j=0;j<4;j++){
+                    int x=cur[0]+dr[j];
+                    int y=cur[1]+dc[j];
+                    if(x>=0 && x<=n-1 && y>=0 && y<=m-1 && mat[x][y]==1){
+                        dist[x][y]=dist[cur[0]][cur[1]]+1;
+                        mat[x][y]=0;
+                        queue.add(new int[]{x,y});
+                    }
+                }
+            }
+        }
+        return dist;
+    }
+}
