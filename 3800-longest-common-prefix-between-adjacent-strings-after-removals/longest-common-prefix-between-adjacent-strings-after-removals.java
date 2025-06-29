cf91bec0828e1d4d313d936bcd5295284e5a21d0
srinath39 <96327215+srinath39@users.noreply.github.com>
2025-06-29 12:30:53 +0530

    Time: 33 ms (29.71%) | Memory: 67 MB (39.11%) - LeetSync

---
+++ 3800-longest-common-prefix-between-adjacent-strings-after-removals/longest-common-prefix-between-adjacent-strings-after-removals.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int[] longestCommonPrefix(String[] words) {
+        int n=words.length;
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            if(i+1<n){
+                arr[i]=lengthOfCommonPrefix(words[i],words[i+1]);
+            }
+            // if(i+2<n){
+            //     arr[i][1]=lengthOfCommonPrefix(words[i],words[i+2],map);
+            // }
+        }
+        int[] suf=new int[n];
+        suf[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            suf[i]=Math.max(suf[i+1],arr[i]);
+        }
+
+        int[] pre=new int[n];
+        pre[0]=arr[0];
+        for(int i=1;i<n;i++){
+            pre[i]=Math.max(pre[i-1],arr[i]);
+        }
+
+        
+        int[] ans=new int[n];
+        for(int i=0;i<n;i++){
+            int max=0;
+            if(i+1<n){
+                max=Math.max(max,suf[i+1]);
+            }
+            if(i-2>=0){
+                max=Math.max(max,pre[i-2]);          
+            }
+            if(i+1<n && i-1>=0){
+                max=Math.max(max,lengthOfCommonPrefix(words[i-1],words[i+1]));
+            }
+            ans[i]=max;
+        }
+        return ans;
+        
+    }
+
+    public int lengthOfCommonPrefix(String str1, String str2){
+        int count=0;
+        int i=0;
+        while(i<str1.length() && i< str2.length()){
+            if(str1.charAt(i)==str2.charAt(i)){
+                count++;
+            }else{
+                break;
+            }
+            i++;
+        }
+        return count;
+    }
+}
