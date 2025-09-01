1ee6e8bdfe6edec084a283997dd826f6044f7995
srinath39 <96327215+srinath39@users.noreply.github.com>
2025-09-01 10:35:12 +0530

    Time: 2 ms (87.03%) | Memory: 55.4 MB (74.89%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,24 +1,38 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n=nums.length;
-        int[] prefixProd=new int[n];
-        prefixProd[0]=nums[0];
-        for(int i=1;i<n;++i){
-            prefixProd[i]=prefixProd[i-1]*nums[i];
-        }
-
-        int[] suffixProd=new int[n];
-        suffixProd[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;--i){
-            suffixProd[i]=suffixProd[i+1]*nums[i];
-        }
-
         int[] ans=new int[n];
-        int leftProd,rightProd;
-        for(int i=0;i<n;++i){
-            leftProd=i==0?1:prefixProd[i-1];
-            rightProd=i==n-1?1:suffixProd[i+1];
-            ans[i]=leftProd*rightProd;
+        // using prefic and suffix 
+
+        // int[] prefixProd=new int[n];
+        // prefixProd[0]=nums[0];
+        // for(int i=1;i<n;++i){
+        //     prefixProd[i]=prefixProd[i-1]*nums[i];
+        // }
+
+        // int[] suffixProd=new int[n];
+        // suffixProd[n-1]=nums[n-1];
+        // for(int i=n-2;i>=0;--i){
+        //     suffixProd[i]=suffixProd[i+1]*nums[i];
+        // }
+
+        // int leftProd,rightProd;
+        // for(int i=0;i<n;++i){
+        //     leftProd=i==0?1:prefixProd[i-1];
+        //     rightProd=i==n-1?1:suffixProd[i+1];
+        //     ans[i]=leftProd*rightProd;
+        // }
+
+
+        ans[0]=1;
+        for(int i=1;i<n;++i){
+            ans[i]=(ans[i-1]*nums[i-1]);
+        }
+
+        int right =1;
+        for(int i=n-2;i>=0;--i){
+            right*=nums[i+1];
+            ans[i]*=right;
         }
         return ans;
     }
