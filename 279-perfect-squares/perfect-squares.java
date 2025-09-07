ceea24e3eb47d497966b1c6288c9feb3f0513bfa
srinath39 <96327215+srinath39@users.noreply.github.com>
2025-09-07 23:43:59 +0530

    Time: 32 ms (70.93%) | Memory: 42.9 MB (63.18%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,39 +1,11 @@
 class Solution {
-    public static int m;
-    public static int[] dp;
     public int numSquares(int n) {
-        // m=0;
-        // for(int i=1;i<=n;i++){
-        //     if(n-(i*i)>=0){
-        //         ++m;
-        //     }else{
-        //         break;
-        //     }
-        // }
-        dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        minSubSetSize(n);
-        return dp[n];
-    }
-
-    public int minSubSetSize(int n){
-        if(n<0){
-            return Integer.MAX_VALUE;
-        }
-        if(n==0){
-            return 0;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int val;
-        dp[n]=Integer.MAX_VALUE;
-        for(int i=1;i<=((int)Math.sqrt(n)+1);++i){
-            if((n-(i*i))>=0){ 
-                val=minSubSetSize(n-(i*i));
-                if(val!=Integer.MAX_VALUE){
-                    dp[n]=Math.min(dp[n],val+1);
-                }
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0]=0;
+        for(int i=1;i<=n;++i){
+            for(int j=1;j*j<=i;++j){
+                dp[i]=Math.min(dp[i],dp[i-(j*j)]+1);
             }
         }
         return dp[n];
