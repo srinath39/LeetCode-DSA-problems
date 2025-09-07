9b9ffc0aa28fc53d7c39537d13cdaae239f493a5
srinath39 <96327215+srinath39@users.noreply.github.com>
2025-09-07 19:52:58 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (78.11%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,35 +1,11 @@
 class Solution {
-    public static int[] arr;
-    public static int n;
-    public static int[] dp;
     public int rob(int[] nums) {
-        arr=nums;
-        n=nums.length;
-        int ans=0;
-        dp=new int[n];
-        Arrays.fill(dp,-1);
-        for(int i=0;i<n;++i){
-            ans=Math.max(ans,maxMoney(i));
+        int n=nums.length;
+        int[] dp=new int[n];
+        dp[0]=nums[0];
+        for(int i=1;i<n;++i){
+            dp[i]=Math.max(((i-2)<0?0:dp[i-2])+nums[i],dp[i-1]);
         }
-        return ans;
-    }
-
-    public int maxMoney(int j){
-        if(j>=n){
-            return 0;
-        }
-        if(dp[j]!=-1){
-            return dp[j];
-        }
-        
-        if(j==n-1 || j==n-2){
-            dp[j]=arr[j];
-            return arr[j];
-        }
-        dp[j]=0;
-        for(int k=j+2;k<n;++k){
-            dp[j]=Math.max(dp[j],maxMoney(k));
-        }
-        return dp[j]=dp[j]+arr[j];
+        return dp[n-1];
     }
 }
