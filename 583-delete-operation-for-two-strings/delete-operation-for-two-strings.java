208a5c07a81669c9810b6b90fd4c33456fd7b11d
srinath39 <96327215+srinath39@users.noreply.github.com>
2025-09-30 20:47:30 +0530

    Time: 7 ms (69.03%) | Memory: 45.3 MB (8.03%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,36 +1,24 @@
 class Solution {
-    public static String A;
-    public static String B;
-    public static int[][] mat;
     public int minDistance(String word1, String word2) {
-        A=word1;
-        B=word2;
         int n=word1.length();
         int m=word2.length();
-        mat=new int[n][m];
-        for(int[] dp:mat){
-            Arrays.fill(dp,-1);
+        int[][] mat=new int[n+1][m+1];
+        for(int i=1;i<=n;++i){
+            mat[i][0]=i;
         }
-        return minDeletionOperations(n-1,m-1);
-    }
-
-    public int minDeletionOperations(int i,int j){
-        if(i<0){
-            return j+1;
+        for(int j=1;j<=m;++j){
+            mat[0][j]=j;
         }
-        if(j<0){
-            return i+1;
+        for(int i=1;i<=n;++i){
+            for(int j=1;j<=m;++j){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    mat[i][j]=mat[i-1][j-1];
+                }
+                else{
+                    mat[i][j]=Math.min(mat[i-1][j],mat[i][j-1])+1;
+                }
+            }
         }
-        if(mat[i][j]!=-1){
-            return mat[i][j];
-        }
-        if(A.charAt(i)==B.charAt(j)){
-            mat[i][j]=minDeletionOperations(i-1,j-1);
-            return mat[i][j];
-        }
-        int delInA= minDeletionOperations(i-1,j)+1;
-        int delInB=minDeletionOperations(i,j-1)+1;
-        mat[i][j]=Math.min(delInA,delInB);
-        return mat[i][j];
+        return mat[n][m];
     }
 }
