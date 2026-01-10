6e65aa4950b8b82538050f741be46b277b3b3984
srinath39 <96327215+srinath39@users.noreply.github.com>
2026-01-10 20:41:37 +0530

    Time: 116 ms (17.29%) | Memory: 45.1 MB (22.48%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,36 +1,33 @@
 class Solution {
-    public int[][] mat;
+    public int[] dp;
     public int[] arr;
+    public int k;
     public int numSquares(int n) {
-        int k=(int)Math.sqrt(n);
+        k=(int)Math.sqrt(n);
         arr=new int[k];
         for(int i=1;i<=k;++i){
             arr[i-1]=i*i;
         }
-        mat=new int[n+1][k];
-        for(int[] dp:mat){
-            Arrays.fill(dp,-1);
-        }
-        return minSubsetSize(n,k-1);
+        dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return minSubsetSize(n);
     }
 
-    public int minSubsetSize(int sum,int i){
-        if(i<0){
+    public int minSubsetSize(int s){
+        if(s<0){
             return Integer.MAX_VALUE;
         }
-        if(sum==0){
+        if(s==0){
             return 0;
         }
-        if(mat[sum][i]!=-1){
-            return mat[sum][i];
+        if(dp[s]!=-1){
+            return dp[s];
         }
-        int x=Integer.MAX_VALUE;
-        int y;
-        if((sum-arr[i])>=0){
-            x=minSubsetSize(sum-arr[i],i)+1;
+        dp[s]=Integer.MAX_VALUE;
+        for(int i=0;i<k;++i){
+            int val=minSubsetSize(s-arr[i]);
+            dp[s]=Math.min(dp[s],val==Integer.MAX_VALUE?val:val+1);
         }
-        y=minSubsetSize(sum,i-1);
-        mat[sum][i]=Math.min(x,y);
-        return mat[sum][i];
+        return dp[s];
     }
 }
