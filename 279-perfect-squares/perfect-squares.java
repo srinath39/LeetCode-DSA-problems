66eeb96116d0c9a0e5a98c1c8adc7e998d15daf1
srinath39 <96327215+srinath39@users.noreply.github.com>
2026-01-10 20:51:13 +0530

    Time: 40 ms (41.58%) | Memory: 44.8 MB (34.04%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,33 +1,41 @@
 class Solution {
-    public int[] dp;
-    public int[] arr;
-    public int k;
+    // public int[] dp;
+    // public int[] arr;
+    // public int k;
     public int numSquares(int n) {
-        k=(int)Math.sqrt(n);
-        arr=new int[k];
+        int k=(int)Math.sqrt(n);
+        int[] arr=new int[k];
         for(int i=1;i<=k;++i){
             arr[i-1]=i*i;
         }
-        dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return minSubsetSize(n);
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0]=0;
+        for(int s=1;s<=n;++s){
+            for(int i=0;i<k;++i){
+                if((s-arr[i])>=0){
+                    dp[s]=Math.min(dp[s],dp[s-arr[i]]+1);
+                }
+            }
+        }
+        return dp[n];
     }
 
-    public int minSubsetSize(int s){
-        if(s<0){
-            return Integer.MAX_VALUE;
-        }
-        if(s==0){
-            return 0;
-        }
-        if(dp[s]!=-1){
-            return dp[s];
-        }
-        dp[s]=Integer.MAX_VALUE;
-        for(int i=0;i<k;++i){
-            int val=minSubsetSize(s-arr[i]);
-            dp[s]=Math.min(dp[s],val==Integer.MAX_VALUE?val:val+1);
-        }
-        return dp[s];
-    }
+    // public int minSubsetSize(int s){
+    //     if(s<0){
+    //         return Integer.MAX_VALUE;
+    //     }
+    //     if(s==0){
+    //         return 0;
+    //     }
+    //     if(dp[s]!=-1){
+    //         return dp[s];
+    //     }
+    //     dp[s]=Integer.MAX_VALUE;
+    //     for(int i=0;i<k;++i){
+    //         int val=minSubsetSize(s-arr[i]);
+    //         dp[s]=Math.min(dp[s],val==Integer.MAX_VALUE?val:val+1);
+    //     }
+    //     return dp[s];
+    // }
 }
