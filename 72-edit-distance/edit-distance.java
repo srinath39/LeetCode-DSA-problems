d0934c52afbe7b24792004887d2a6c264db777d3
srinath39 <96327215+srinath39@users.noreply.github.com>
2026-01-14 08:39:22 +0530

    Time: 5 ms (65.63%) | Memory: 47.2 MB (58.61%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,34 +1,24 @@
 class Solution {
-    public static int[][] mat;
     public int minDistance(String word1, String word2) {
         int n=word1.length();
         int m=word2.length();
-        if(n==0){
-            return m;
+        int[][] mat=new int[n+1][m+1];
+        for(int i=0;i<=m;++i){
+            mat[0][i]=i;
         }
-        if(m==0){
-            return n;
+        for(int j=0;j<=n;++j){
+            mat[j][0]=j;
         }
-        mat=new int[n][m];
-        for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                if(word1.charAt(i)==word2.charAt(j)){
-                    mat[i][j]=getValue(i-1,j-1);
+        for(int i=1;i<=n;++i){
+            for(int j=1;j<=m;++j){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    mat[i][j]=mat[i-1][j-1];
                 }else{
-                    mat[i][j]=Math.min(getValue(i-1,j),Math.min(getValue(i,j-1),getValue(i-1,j-1)))+1;
+                    mat[i][j]=Math.min(mat[i-1][j],Math.min(mat[i][j-1],mat[i-1][j-1]))+1;
                 }
             }
         }
-        return mat[n-1][m-1];
+        return mat[n][m];
     }
 
-    public int getValue(int i,int j){
-        if(i<0){
-            return j+1;
-        }
-        if(j<0){
-            return i+1;
-        }
-        return mat[i][j];
-    }
 }
