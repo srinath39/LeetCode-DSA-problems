3e3a7222fa0c914c308b57b5c6d7470d6f7249de
srinath39 <96327215+srinath39@users.noreply.github.com>
2026-01-14 12:39:38 +0530

    Time: 21 ms (37.73%) | Memory: 46.6 MB (32.85%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,54 +1,25 @@
 class Solution {
-    public static String A;
-    public static String B;
-    public static int[][] mat;
-    public static int[] presumA;
-    public static int[] presumB;
     public int minimumDeleteSum(String s1, String s2) {
-        A=s1;
-        B=s2;
         int n=s1.length();
         int m=s2.length();
-
-        presumA=new int[n];
-        presumA[0]=s1.charAt(0);
-        for(int i=1;i<n;++i){
-            presumA[i]=presumA[i-1]+s1.charAt(i);
+        int[][] mat=new int[n+1][m+1];
+        for(int i=1;i<=m;++i){
+            mat[0][i]=mat[0][i-1]+s2.charAt(i-1);
         }
-
-        presumB=new int[m];
-        presumB[0]=s2.charAt(0);
-        for(int i=1;i<m;++i){
-            presumB[i]=presumB[i-1]+s2.charAt(i);
+        for(int i=1;i<=n;++i){
+            mat[i][0]=mat[i-1][0]+s1.charAt(i-1);
         }
-
-        mat=new int[n][m];
-        for(int[] dp:mat){
-            Arrays.fill(dp,-1);
+        for(int i=1;i<=n;++i){
+            for(int j=1;j<=m;++j){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    mat[i][j]=mat[i-1][j-1];
+                }else{
+                    mat[i][j]=Math.min(mat[i-1][j]+s1.charAt(i-1),mat[i][j-1]+s2.charAt(j-1));
+                }
+            }
         }
-        return minAsciiSumDeletionOperations(n-1,m-1);
+        return mat[n][m];
     }
 
-    public int minAsciiSumDeletionOperations(int i,int j){
-        if(i<0 && j<0){
-            return i+1;
-        }
-        if(i<0){
-            return presumB[j];
-        }
-        if(j<0){
-            return presumA[i];
-        }
-        if(mat[i][j]!=-1){
-            return mat[i][j];
-        }
-        if(A.charAt(i)==B.charAt(j)){
-            mat[i][j]=minAsciiSumDeletionOperations(i-1,j-1);
-            return mat[i][j];
-        }
-        int delInA= minAsciiSumDeletionOperations(i-1,j)+A.charAt(i);
-        int delInB=minAsciiSumDeletionOperations(i,j-1)+B.charAt(j);
-        mat[i][j]=Math.min(delInA,delInB);
-        return mat[i][j];
+    
     }
-}
