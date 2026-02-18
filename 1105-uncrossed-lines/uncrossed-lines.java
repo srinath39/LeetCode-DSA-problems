0793e3b9a11930f22381e585394987c13a6a4801
srinath39 <96327215+srinath39@users.noreply.github.com>
2026-02-18 15:03:25 +0530

    Time: 6 ms (82.26%) | Memory: 46.3 MB (24.72%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.java
+++ 1105-uncrossed-lines/uncrossed-lines.java
@@ -2,16 +2,16 @@ class Solution {
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
         int n=nums1.length;
         int m=nums2.length;
-        int[][] mat=new int[n+1][m+1];
+        int[][] dp=new int[n+1][m+1];
         for(int i=1;i<=n;++i){
             for(int j=1;j<=m;++j){
                 if(nums1[i-1]==nums2[j-1]){
-                    mat[i][j]=mat[i-1][j-1]+1;
+                    dp[i][j]=dp[i-1][j-1]+1;
                 }else{
-                    mat[i][j]=Math.max(mat[i-1][j],mat[i][j-1]);
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-        return mat[n][m];
+        return dp[n][m];
     }
 }
