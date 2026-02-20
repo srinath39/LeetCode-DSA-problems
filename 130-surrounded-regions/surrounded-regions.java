a144ee8eb2f4f08779bd5c7046bc48bf40ca21ce
srinath39 <96327215+srinath39@users.noreply.github.com>
2026-02-20 09:11:17 +0530

    Time: 4 ms (18.70%) | Memory: 47.5 MB (92.10%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -2,30 +2,31 @@ class Solution {
     public void solve(char[][] board) {
         int m=board.length;
         int n=board[0].length;
-        Queue<int[]> q=new LinkedList<>();
         boolean[][] visited=new boolean[m][n];
+        Queue<int[]> q=new LinkedList<>();
         for(int i=0;i<m;++i){
             for(int j=0;j<n;++j){
-                if(board[i][j]=='O' && (i==0 || i==m-1 || j==0 || j==n-1)){
-                    q.add(new int[]{i,j});
-                    visited[i][j]=true;
+                if(board[i][j]=='O'){
+                    if(i==0 || i==m-1 || j==0 || j==n-1){
+                        q.add(new int[]{i,j});
+                        visited[i][j]=true;
+                    }
                 }
             }
         }
-        int[] dr={-1,1,0,0};
-        int[] dc={0,0,-1,1};
+        int[] dx={-1,1,0,0};
+        int[] dy={0,0,-1,1};
         while(!q.isEmpty()){
             int[] cur=q.remove();
             for(int i=0;i<4;++i){
-                int x=cur[0]+dr[i];
-                int y=cur[1]+dc[i];
-                if(x>=0 && x<=m-1 && y>=0 && y<=n-1 && !visited[x][y] && board[x][y]=='O'){
+                int x=cur[0]+dx[i];
+                int y=cur[1]+dy[i];
+                if(x>=0 && x<=m-1 && y>=0 && y<=n-1 && board[x][y]=='O' && !visited[x][y]){
                     visited[x][y]=true;
                     q.add(new int[]{x,y});
                 }
             }
         }
-
         for(int i=0;i<m;++i){
             for(int j=0;j<n;++j){
                 if(board[i][j]=='O' && !visited[i][j]){
@@ -34,5 +35,4 @@ class Solution {
             }
         }
     }
-
 }
