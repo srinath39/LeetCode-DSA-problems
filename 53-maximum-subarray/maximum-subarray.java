6ee337c065d6686a509ec1ba4f7c4f0417b18af1
srinath39 <96327215+srinath39@users.noreply.github.com>
2026-02-20 19:53:28 +0530

    Time: 1 ms (99.68%) | Memory: 79 MB (13.31%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -6,7 +6,7 @@ class Solution {
         dp[0]=nums[0];
         int ans=nums[0];
         for(int i=1;i<n;++i){
-           dp[i]=Math.max(dp[i-1]+nums[i],nums[i]);
+           dp[i]=Math.max(dp[i-1],0)+nums[i];
            ans=Math.max(ans,dp[i]);
         }
         return ans;
