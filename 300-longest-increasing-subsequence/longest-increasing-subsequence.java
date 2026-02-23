5028961ac19286556390d9abeca092d0994828db
srinath39 <96327215+srinath39@users.noreply.github.com>
2026-02-23 17:15:49 +0530

    Time: 48 ms (38.81%) | Memory: 46.3 MB (42.35%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,31 +1,21 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n=nums.length;
-        int[] arr=new int[n];
-        int p=0;
-        arr[p++]=nums[0];
+        int[] dp=new int[n];
+        Arrays.fill(dp,1);
+        int ans=1;
+        int[] parent=new int[n];
         for(int i=1;i<n;++i){
-            int ele=arr[p-1];
-            if(ele<nums[i]){
-                arr[p++]=nums[i];
-            }else{
-                setElementInList(arr,nums[i],p);
+            int max=0;
+            for(int j=0;j<i;++j){
+                if(nums[j]<nums[i] && dp[j]>max){
+                    max=dp[j];
+                    dp[i]=max+1;
+                    parent[i]=max;
+                }
             }
+            ans=Math.max(ans,dp[i]);
         }
-        return p;
-    }
-
-    public void setElementInList(int[] arr,int k,int p){
-        int L=-1;
-        int R=p-1;
-        while((L+1)<R){
-            int m=(L+R)/2;
-            if(arr[m]<k){
-                L=m;
-            }else{
-                R=m;
-            }
-        }
-        arr[R]=k;
+        return ans;
     }
 }
