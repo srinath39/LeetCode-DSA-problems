1a519fe007074b53e33628239661e43a697429e4
srinath39 <96327215+srinath39@users.noreply.github.com>
2026-03-03 17:14:01 +0530

    Time: 1 ms (99.82%) | Memory: 46.6 MB (37.07%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,56 +10,25 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int m=getLength(l1);
-        int n=getLength(l2);
         ListNode dummy=new ListNode();
-        ListNode head1,head2;
-        head1=dummy;
-        if(m<=n){
-            dummy.next=l2;
-            head2=l1;
-        }else{
-            dummy.next=l1;
-            head2=l2;
-        }
-        int c=0;
-        while(head2!=null){
-            int val=head1.next.val+head2.val+c;
-            if(val>9){
-                head1.next.val=val%10;
-                c=1;
-            }else{
-                head1.next.val=val;
-                c=0;
+        ListNode temp=dummy;
+        int a,b,c=0;
+        while(l1!=null || l2!=null || c>0){
+            a=0;b=0;
+            if(l1!=null){
+                a=l1.val;
+                l1=l1.next;
             }
-            head2=head2.next;
-            head1=head1.next;
-        }
-
-        while(c>0 && head1.next!=null){
-            int val=head1.next.val+c;
-            if(val>9){
-                head1.next.val=val%10;
-                c=1;
-            }else{
-                head1.next.val=val;
-                c=0;
+            if(l2!=null){
+                b=l2.val;
+                l2=l2.next;
             }
-            head1=head1.next;
-        }
-
-        if(c>0){
-            head1.next=new ListNode(c);
+            int val=a+b+c;
+            ListNode nn=new ListNode(val%10);
+            temp.next=nn;
+            temp=nn;
+            c=val/10;
         }
         return dummy.next;
-    }
-
-    public int getLength(ListNode l){
-        int len=0;
-        while(l!=null){
-            ++len;
-            l=l.next;
-        }
-        return len;
     }
 }
