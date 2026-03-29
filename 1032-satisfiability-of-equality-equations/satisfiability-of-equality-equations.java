222561110925bb268bad44d58ff00a94b62e4dba
srinath39 <96327215+srinath39@users.noreply.github.com>
2026-03-29 21:22:33 +0530

    Time: 1 ms (97.36%) | Memory: 43.8 MB (64.08%) - LeetSync

--- 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
@@ -1,49 +1,40 @@
 class Solution {
+    public static int[] parent;
     public boolean equationsPossible(String[] equations) {
         int n=equations.length;
-        ArrayList<char[]> equalsStrings=new ArrayList<>();
-        ArrayList<char[]> notEqualsString=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(equations[i].charAt(1)=='!'){
-                notEqualsString.add(new char[]{equations[i].charAt(0),equations[i].charAt(3)});
-            }else{
-                equalsStrings.add(new char[]{equations[i].charAt(0),equations[i].charAt(3)});
+        parent=new int[26];
+        Arrays.fill(parent,-1);
+        for(int i=0;i<n;++i){
+            if(equations[i].charAt(1)=='='){
+                merge(equations[i].charAt(0)-97,equations[i].charAt(3)-97);
             }
         }
 
-        int[] parent=new int[26];
-        Arrays.fill(parent,-1);
-        for(int i=0;i<equalsStrings.size();i++){
-            merge(equalsStrings.get(i)[0]-97,equalsStrings.get(i)[1]-97,parent);
-        }
-
-
-        for(int i=0;i<notEqualsString.size();i++){
-            if(findLeader(notEqualsString.get(i)[0]-97,parent)==findLeader(notEqualsString.get(i)[1]-97,parent)){
+        for(int i=0;i<n;++i){
+            if(equations[i].charAt(1)=='!' && findLeader(equations[i].charAt(0)-97)==findLeader(equations[i].charAt(3)-97)){
                 return false;
             }
         }
-
         return true;
-
     }
 
-    public int findLeader(int v,int[] parent){
-        if(parent[v]<0){
-            return v;
+    public void merge(int x,int y){
+        if((x=findLeader(x))==(y=findLeader(y))){
+            return;
         }
-        return parent[v]=findLeader(parent[v],parent);
+        if(parent[y]<parent[x]){
+            int t=y;
+            y=x;
+            x=t;
+        }
+        parent[x]+=parent[y];
+        parent[y]=x;
     }
 
-    public void merge(int x,int y,int[] parent){
-        if((x=findLeader(x,parent))!=(y=findLeader(y,parent))){
-            if(parent[x]>parent[y]){
-                int temp=x;
-                x=y;
-                y=temp;
-            }
-            parent[x]+=parent[y];
-            parent[y]=x;
+    public int findLeader(int x){
+        if(parent[x]<0){
+            return x;
         }
+        return parent[x]=findLeader(parent[x]);
     }
 }
