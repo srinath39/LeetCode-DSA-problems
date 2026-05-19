538f57e88559bfbe86f7a3514f058c484a7d0a47
srinath39 <96327215+srinath39@users.noreply.github.com>
2026-05-19 08:40:08 +0530

    Time: 8 ms (96.50%) | Memory: 47.2 MB (5.19%) - LeetSync

--- 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.java
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.java
@@ -1,18 +1,16 @@
 class Solution {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
-        int n=boxTypes.length;
-        Arrays.sort(boxTypes,new Comparator<int[]>(){
-            @Override
-            public int compare(int[] a,int[] b){
-                return b[1]-a[1];
-            }
-        });
-        int min,count=0;
-        for(int i=0;i<n&&truckSize>0;i++){
-            min=Math.min(truckSize,boxTypes[i][0]);
-            count+=(boxTypes[i][1]*min);
-            truckSize-=min;
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->b[1]-a[1]);
+        for(int[] entry :boxTypes){
+            pq.add(entry);
         }
-        return count;
+        int maxUnits=0;
+        while(truckSize>0 && !pq.isEmpty()){
+            int[] cur=pq.remove();
+            int tb=Math.min(truckSize,cur[0]);
+            maxUnits+=(tb*cur[1]);
+            truckSize-=tb;
+        }
+        return maxUnits;
     }
 }
